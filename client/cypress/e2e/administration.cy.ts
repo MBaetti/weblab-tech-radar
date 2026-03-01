5e84d9c716d64ebb697e7972326eac4d3bfb8b2b
MBaetti <maurice.baettig@stud.hslu.ch>
2026-03-01 20:00:26 +0100

    test(e2e)

--- client/cypress/e2e/administration.cy.ts
+++ client/cypress/e2e/administration.cy.ts
@@ -1,14 +1,38 @@
-describe('template spec', () => {
-  it('Load Cypress Viewer', () => {
-    cy.visit('/viewer')
-  });
-
-  it('Load Tech-Viewer', function() {
-    cy.visit('http://localhost:4200/viewer')
-
-  });
+describe('Tests for the Tech-Administration Page', () => {
   it('Load Tech-Administration', function() {
     cy.visit('http://localhost:4200/administration')
+
+  });
+
+  it('Create Technology', function() {
+    cy.visit('http://localhost:4200/administration')
+
+    cy.get('tech-form-component mat-form-field').contains('Name').click();
+    cy.get('tech-form-component input[formcontrolname="nameFormControl"], tech-form-component input[type="text"]').first().type('Test');
+    cy.get('tech-form-component mat-select').first().click();
+    cy.get('mat-option').first().click();
+    cy.get('tech-form-component textarea').first().type('Test');
+    cy.get('tech-form-component button[type="submit"]').contains('Technologie erfassen').click();
+
+  });
+
+  it('Publish Technology', function() {
+    cy.visit('http://localhost:4200/administration')
+
+    cy.get('mat-card').last().find('button').contains('Publizieren').click();
+    cy.get('#techDialog mat-select').click();
+    cy.get('mat-option').first().click();
+    cy.get('#techDialog textarea').click({force: true});
+    cy.get('#techDialog textarea').type('Test');
+    cy.get('mat-dialog-actions button').contains('Technologie publizieren').click();
+    cy.get('mat-dialog-actions button').contains('Schliessen').click();
+
+  });
+
+  it('Delete Technology', function() {
+    cy.visit('http://localhost:4200/administration')
+
+    cy.get('mat-card').last().find('button').contains('Löschen').click();
 
   });
 });
---
+++ client/cypress/e2e/viewer.cy.ts
@@ -0,0 +1,5 @@
+describe('Tests for the Tech-Viewer Page', () => {
+  it('Load Tech-Viewer', function() {
+    cy.visit('http://localhost:4200/viewer')
+  });
+})
